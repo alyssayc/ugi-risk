b1eb3bf6552db2fe35e34769162652fe50a1d3cf
Alyssa <alyssayc14@gmail.com>
2024-06-12 19:08:31 -0400

    nontemporal demographic feature extraction from omop

--- sql/main.sql
+++ sql/main.sql
@@ -1,1 +1,11 @@
-SELECT * FROM omop.cdm LIMIT 10;
+SELECT TOP 10
+	person_id as pt_id,
+	gender_c.concept_name as sex,
+	DATEFROMPARTS(year_of_birth, month_of_birth, day_of_birth) as dob,
+	race_c.concept_name as race,
+	ethnicity_c.concept_name as ethnicity,
+	p.xtn_patient_epic_mrn as mrn
+FROM omop.cdm_phi.person p 
+INNER JOIN omop.cdm_phi.concept gender_c ON gender_c.concept_id = p.gender_concept_id 
+INNER JOIN omop.cdm_phi.concept race_c ON race_c.concept_id = p.race_concept_id
+INNER JOIN omop.cdm_phi.concept ethnicity_c ON ethnicity_c.concept_id = p.ethnicity_concept_id 
