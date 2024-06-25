4780512b97d1c06f12ff1bb0c79d962b1dac837e
Alyssa <alyssayc14@gmail.com>
2024-06-25 14:55:27 -0400

    bmi table

--- sql/main.sql
+++ sql/main.sql
@@ -1,11 +1,25 @@
-SELECT TOP 10
+DROP TABLE IF EXISTS #Demographics;
+SELECT TOP 1000
 	person_id as pt_id,
 	gender_c.concept_name as sex,
 	DATEFROMPARTS(year_of_birth, month_of_birth, day_of_birth) as dob,
 	race_c.concept_name as race,
 	ethnicity_c.concept_name as ethnicity,
 	p.xtn_patient_epic_mrn as mrn
+INTO #Demographics
 FROM omop.cdm_phi.person p 
 INNER JOIN omop.cdm_phi.concept gender_c ON gender_c.concept_id = p.gender_concept_id 
 INNER JOIN omop.cdm_phi.concept race_c ON race_c.concept_id = p.race_concept_id
 INNER JOIN omop.cdm_phi.concept ethnicity_c ON ethnicity_c.concept_id = p.ethnicity_concept_id 
+
+DROP TABLE IF EXISTS #BMI;
+SELECT TOP 1000
+	person_id as pt_id,
+	measurement_concept_id as bmi_id,
+	measurement_source_value as bmi_textid,
+	measurement_date as bmi_date,
+	value_as_number as bmi_num,
+	value_source_value as bmi_value,
+	unit_concept_code as bmi_unit
+FROM omop.cdm_phi.measurement
+WHERE measurement_concept_id IN (3038553)
