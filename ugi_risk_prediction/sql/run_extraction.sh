8f06eaedcf0c6cc60c74b17d4bede3133c3cab3e
Alyssa Chen <chena60@li03c04.chimera.hpc.mssm.edu>
2024-09-01 13:01:10 -0400

    include command line arguments and create executable bash script

---
+++ ugi_risk_prediction/sql/run_extraction.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Can add as many command lines as needed 
+# python3 extract_batch.py batch_2016-01-01_2016-12-31.sql data_20160101_20161231.csv.gz
+python3 extract_batch.py batch_2017-01-01_2017-12-31.sql data_20170101_20171231.csv.gz
