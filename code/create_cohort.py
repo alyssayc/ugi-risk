516aabd6ba952a4718f06212ee94f43ec6920d79
Alyssa <alyssayc14@gmail.com>
2025-04-06 17:59:46 -0400

    apply exclusion criteria and clean cohort

--- code/create_cohort.py
+++ code/create_cohort.py
@@ -3,198 +3,239 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from pathlib import Path
-from tableone import TableOne, load_dataset
-import dask.dataframe as dd
 import json
 from datetime import datetime
 import argparse 
 import utils 
 
-def apply_inclusion_criteria(data_dir, data_txt_file):
-    all_mrns = set() # Running count of all MRNs 
-    cohort_mrns = set() # Running count of all MRNs included in cohort
-    past_encs = {} # Dict to store visit start dates for each MRN 
-    cohort = [] # List to accumulate rows for the cohort DataFrame 
+def apply_exclusion_criteria(data_dir, file_list, ugi_file, consort_diagram_numbers_filename):
+    list_chunks = [] # To store the list of data chunks
 
-    # Date variables to convert to datetime
-    date_variables = ['visit_start_date', 'visit_start_date_minus_18mo', 'visit_start_date_minus_6mo', 'gastricca_start_date', 'esophagealca_start_date']
+    # Keep track of number of encounters and unique patients 
+    num_enc_incl = 0
+    pts_incl = set()
+    num_enc_excl_age = 0
 
-    with open(data_dir / data_txt_file, 'r') as file:
-        file_list = [line.strip() for line in file if line.strip()]
+    for filename in file_list:
+        df_chunk = pd.read_csv(data_dir / filename, low_memory=False)[utils.COLNAMES_COHORT]
+        print(f'{filename}: {df_chunk.shape}')
 
-        for filename in file_list:
-            df_chunk = pd.read_csv(data_dir / filename, low_memory=False)
-            print(f'{filename}: {df_chunk.shape}')
-            
-            # Set the column names for the final dataframe - only need to do this once 
-            if len(cohort_cols) == 0:
-                cohort_cols = df_chunk.columns  
+        # Update numbers 
+        num_enc_incl += df_chunk.shape[0]
+        pts_incl.update(df_chunk.pt_id)
 
-            # Create a running set of all MRNs that met the study period 
-            all_mrns.update(set(df_chunk.mrn))
+        # Apply screening age exclusion criteria 
+        excl_age = (df_chunk.age < 40) | (df_chunk.age > 85) # Pts not within proposed screening age of 40-85 
+        num_enc_excl_age += int(excl_age.sum())
+        list_chunks.append(df_chunk[~excl_age]) 
 
-            # Sort from earliest to latest date because the algorithm will check to see if there was a prior encounter 
-            df_chunk.sort_values(by='visit_start_date', ascending=True, inplace=True) 
+    # Merge the data chunks together in one dataframe
+    df_chunks = pd.concat(list_chunks, ignore_index=True)
 
-            # Convert to datetime objects
-            for date_col in date_variables:
-                # Print the number of nulls before and after converting to see if there are errors with conversion.
-                # coercing errors forces values that are not datetime into null.
-                print(f'Number of nulls before converting: {df_chunk[date_col].isna().sum()}')
-                df_chunk[date_col] = pd.to_datetime(df_chunk[date_col], errors='coerce') 
-                print(f'Number of nulls after converting: {df_chunk[date_col].isna().sum()}')
+    # Apply multiple encounters exclusion criteria 
+    df_patients = df_chunks.sort_values(by='visit_start_date').drop_duplicates(subset='pt_id', keep='first')
+    num_enc_excl_duplicates = df_chunks.shape[0] - df_patients.shape[0]
+    num_pt_excl_duplicates = df_chunks.pt_id.nunique() - df_patients.pt_id.nunique()
 
-            # Brute force through each patient and apply inclusion criteria
-            # Inclusion criteria - Age 40-85 and has a prior encounter
-            for idx, row in df_chunk.iterrows():
-                mrn = row.mrn
-                age = row.age
-                visit_start_date = row.visit_start_date
+    # Read UGI cancer registry data 
+    df_ugi = pd.read_csv(data_dir / ugi_file, low_memory=False) #, index_col = False)
 
-                if mrn in cohort_mrns: # If MRN is already included, then not first encounter pair so exclude 
-                    continue
-                elif mrn in past_encs and age >= 40 and age <= 85: # Include first encounter pair 
-                    cohort.append(row)
-                    cohort_mrns.add(mrn)
-                else: # First encounter for this mrn 
-                    past_encs[mrn] = [visit_start_date]
-    
-    print('Applying inclusion criteria')
-    print(f'Total MRNs: {len(all_mrns)}\nMRNs that meet inclusion criteria: {len(cohort_mrns)}')
+    # Select the first UGI cancer diagnosis by date for patients with multiple UGI cancers 
+    df_ugi_first = df_ugi.sort_values(by='datetime_dx').drop_duplicates(subset='mrn', keep='first')
 
-    # Turn into a dataframe 
-    return pd.DataFrame(cohort, columns=cohort_cols)
+    # Merge UGI registry with extracted Epic data 
+    df_merged = df_patients.merge(df_ugi_first, on='mrn', how='left')
 
-def apply_exclusion_critera(df_merged):
+    print(f'Number of patients in registry without matching MRN: {df_ugi_first.shape[0] - df_merged.datetime_dx.notnull().sum()}')
+    print(f'Number of patients in registry with matching MRN: {int(((df_merged.pt_id.notnull()) & df_merged.datetime_dx.notnull()).sum())}')
+
+    # Convert height from inches to meters and weight from ounces to kg
+    inches_to_meters = 0.0254
+    ounces_to_kg = 0.0283495
+
+    df_merged['height_baseline_m'] = df_merged['height_baseline'] * inches_to_meters
+    df_merged['weight_baseline_kg'] = df_merged['weight_baseline'] * ounces_to_kg
+    df_merged['BMI_baseline_all'] = df_merged['weight_baseline_kg'] / df_merged['height_baseline_m']**2
+
+    # Count and calculate the percentage of non-null values
+    columns = ["height_baseline", "weight_baseline", "BMI_baseline", 'BMI_baseline_all']
+    non_null_counts = df_merged[columns].notnull().sum()
+    non_null_percentages = (non_null_counts / len(df_merged)) * 100
+
+    summary = pd.DataFrame({
+        "non_null_count": non_null_counts,
+        "non_null_percent": non_null_percentages.round(2)
+    })
+    print(summary)
+
+    # Convert to datetime 
+    date_vars = ['visit_start_date', 'datetime_dx', 'date_of_death']
+    for date_var in date_vars: 
+        df_merged[date_var] = pd.to_datetime(df_merged[date_var], errors='coerce')
+
     # Define exclusion criteria 
-    # excl_age = (df_merged.age < 40) | (df_merged.age > 85) # Pts not within proposed screening age of 40-85 
     excl_dx_before_visit = (df_merged.visit_start_date > df_merged.datetime_dx) # Pts whose UGI cancer diagnosis occured before the visit date
+    excl_dx_soon_after_visit = ((df_merged.visit_start_date + pd.DateOffset(months=12)) >= df_merged.datetime_dx) # Pts whose UGI cancer diagnosis occured less than 12 months after the visit date
     excl_gastrichx = (df_merged.gastricca == 1.0) & (df_merged.primary_tumor_site.isna()) # Pts who had a hx of gastric cancer as part of their PMHx but not included in the registry
     excl_esophagealhx = (df_merged.esophagealca == 1.0) & (df_merged.primary_tumor_site.isna()) # Pts who had a hx of esophageal cancer as part of their PMHx but not included in the registry
+    excl_otherugicahx = (df_merged.ugica_other == 1) # Pts who were in the registry for other UGI cancer 
+    excl_death = (df_merged.visit_start_date <= df_merged.date_of_death) # Pts whose death date is documented as prior to or day of encounter date
+    excl_bmi_missing = (df_merged.BMI_baseline_all.isna()) # Pts who has missing BMI, aka not seen in person for the last 6 months 
 
-    # Calculate number excluded 
-    num_incl = df_merged.shape[0]
-    # num_excl_age = excl_age.sum()
-    num_excl_dx_before_visit = excl_dx_before_visit.sum()
-    num_excl_gastrichx = excl_gastrichx.sum()
-    num_excl_esophagealhx = excl_esophagealhx.sum()
+    # Calculate number of pts excluded
+    num_pt_excl_age = len(pts_incl) - df_chunks.pt_id.nunique()
+    num_pt_excl_dx_before_visit = excl_dx_before_visit.sum()
+    num_pt_excl_dx_soon_after_visit = excl_dx_soon_after_visit.sum()
+    num_pt_excl_gastrichx = excl_gastrichx.sum()
+    num_pt_excl_esophagealhx = excl_esophagealhx.sum()
+    num_pt_excl_otherugicahx = excl_otherugicahx.sum()
+    num_pt_excl_death = excl_death.sum()
+    num_pt_excl_bmi_missing = excl_bmi_missing.sum()
 
-    print('Applying exclusion criteria')
-    # Print out the number of excluded rows for each criterion
-    print(f"Included: {num_incl}")
-    # num_incl -= num_excl_age
-    # print(f"Excluded due to age: -{num_excl_age} = {num_incl}")
-    num_incl -= num_excl_dx_before_visit
-    print(f"Excluded due to diagnosis before visit: -{num_excl_dx_before_visit} = {num_incl}")
-    num_incl = num_incl - num_excl_gastrichx - num_excl_esophagealhx
-    print(f"Excluded due to UGI cancer history: -{num_excl_gastrichx+num_excl_esophagealhx} = {num_incl}")
+    # Apply exclusion criteria 
+    df_cohort = df_merged[~(excl_dx_before_visit | excl_dx_soon_after_visit | excl_gastrichx | excl_esophagealhx | excl_otherugicahx | excl_death | excl_bmi_missing)]
 
-    # Apply exclusion criteria
-    df_cohort = df_merged[~(excl_dx_before_visit | excl_gastrichx | excl_esophagealhx)]
-    remaining_shape = df_cohort.shape
-    print(f"Remaining rows after applying all exclusions (note one pt can meet multiple criteria): {remaining_shape[0]}")
+    # Total (non-sequential) exclusion numbers for cohort
+    output = (
+        f'Total encounters: {num_enc_incl}, total patients: {len(pts_incl)}\n'
+        f'Excluded - outside screening age: {num_enc_excl_age} encounters, {num_pt_excl_age} pts\n'
+        f'Excluded - multiple encounters: {num_enc_excl_duplicates} encounters, {num_pt_excl_duplicates} pts\n'
+        f'Excluded - UGI cancer dx before visit: {num_pt_excl_dx_before_visit} patients\n'
+        f'Excluded - UGI cancer dx soon after visit: {num_pt_excl_dx_soon_after_visit} patients\n'
+        f'Excluded - gastric ca hx not confirmed: {num_pt_excl_gastrichx} patients\n'
+        f'Excluded - esophageal ca hx not confirmed: {num_pt_excl_esophagealhx} patients\n'
+        f'Excluded - other UGI cancer subtype: {num_pt_excl_otherugicahx} patients\n'
+        f'Excluded - death prior to enc: {num_pt_excl_death} patients\n'
+        f'Excluded - BMI missing: {num_pt_excl_bmi_missing} patients\n'
+        f'Cohort: {df_cohort.shape[0]} encounters, {df_cohort.pt_id.nunique()} patients\n'
+    )
+
+    print(output)
+
+    # Write the output string to a file
+    with open(data_dir / consort_diagram_numbers_filename, "w") as file:
+        file.write(output)
 
     return df_cohort
 
 def clean_data(df):
+    df.visit_start_date = pd.to_datetime(df.visit_start_date)
+    df.datetime_dx = pd.to_datetime(df.datetime_dx)
+    df.date_of_death = pd.to_datetime(df.date_of_death, format='mixed')
+
     # Extract year from visit dates
-    df.loc[:, 'visit_year'] = pd.to_datetime(df.visit_start_date).dt.year
-    df.loc[:, 'diagnosis_year'] = pd.to_datetime(df.datetime_dx).dt.year
+    df['visit_year'] = df.visit_start_date.dt.year
+    df['diagnosis_year'] = df.datetime_dx.dt.year
+    df['death_year'] = df.date_of_death.dt.year
 
     # Clean up race variable 
     df['race_clean'] = df.race.str.lower().map(utils.RACE_DICT)
 
     # Create two cleaned H pylori variables
-    df.loc[:, 'hpylori_active'] = df.apply(utils.clean_hpylori, axis=1) # Only stool and breath testing
-    df.loc[:, 'hpylori_active_chronic'] = df.apply(utils.clean_hpylori_serology, axis=1) # Incorporate Hpylori serology and PMHx
+    # Impute missing = 0 
+    df['hpylori_active'] = df.apply(utils.clean_hpylori, axis=1) # Only stool and breath testing
+    df['hpylori_active_chronic_missing'] = df.apply(utils.clean_hpylori_serology, axis=1) # Incorporate Hpylori serology and PMHx
+    df['hpylori_active_chronic_binary'] = df['hpylori_active_chronic_missing'].apply(lambda x: 1 if x==1 else 0)
+    df['hpylori_active_chronic'] = df['hpylori_active_chronic_missing'].apply(lambda x: x if x in [0, 1] else -1)
 
     # Create a comprehensive tobacco and alcohol variable by merging data obtained from PMHx and social history.
-    df.loc[:, 'tobacco_all'] = df[['tobacco', 'social_smoking_ever']].max(axis=1)
-    df.loc[:, 'alcohol_all'] = df[['alcohol', 'social_alcohol']].max(axis=1)
+    df['tobacco_all'] = df[['tobacco', 'social_smoking_ever']].max(axis=1)
+    df['alcohol_all'] = df[['alcohol', 'social_alcohol']].max(axis=1)
 
-    # Create a change variable for all laboratory data
-    for lab in utils.LAB_VARS: 
-        df.loc[:, f'{lab}_change'] = df[f'{lab}_baseline'] - df[f'{lab}_prior']
+    # Create additional vars for alcohol and tobacco to analyze 
+    df['alcohol_all_missing'] = df['alcohol_all'].apply(lambda x: np.nan if x == -1 else x)
+    df['alcohol_binary_missing'] = df['alcohol_all_missing'].apply(lambda x: 1 if x == 2 else x)
+    df['alcohol_binary'] = df['alcohol_all'].apply(lambda x: 1 if x in [1,2] else 0)
+
+    df['tobacco_all_missing'] = df['tobacco_all'].apply(lambda x: np.nan if x == -1 else x)
+    df['tobacco_binary_missing'] = df['tobacco_all_missing'].apply(lambda x: 1 if x == 2 else x)
+    df['tobacco_binary'] = df['tobacco_all'].apply(lambda x: 1 if x in [1,2] else 0)
+
+    # Make PMHx and FMHx binary 
+    df['hnca'] = df['hnca'].apply(lambda x: 1 if x == 1 else 0)
+    df['achalasia'] = df['achalasia'].apply(lambda x: 1 if x == 1 else 0)
+    df['pud'] = df['pud'].apply(lambda x: 1 if x == 1 else 0)
+    df['gerd'] = df['gerd'].apply(lambda x: 1 if x == 1 else 0)
+    df['barretts'] = df['barretts'].apply(lambda x: 1 if x == 1 else 0)
+    df['cad'] = df['cad'].apply(lambda x: 1 if x == 1 else 0)
+    df['famhx_cancer'] = df['famhx_cancer'].apply(lambda x: 1 if x == 1 else 0)
+    df['famhx_gastricca'] = df['famhx_gastricca'].apply(lambda x: 1 if x == 1 else 0)
+    df['famhx_esophagealca'] = df['famhx_esophagealca'].apply(lambda x: 1 if x == 1 else 0)
+    df['famhx_colonca'] = df['famhx_colonca'].apply(lambda x: 1 if x == 1 else 0)
+    df['famhx_barretts'] = df['famhx_barretts'].apply(lambda x: 1 if x == 1 else 0)
 
     # Categorize medication use date
-    df.loc[:, 'PPI'] = df['PPI_start_date'].notna().astype(int)
-    df.loc[:, 'ASA'] = df['ASA_start_date'].notna().astype(int)
-    df.loc[:, 'NSAID'] = df['NSAID_start_date'].notna().astype(int)
+    df['PPI'] = df['PPI_start_date'].notna().astype(int)
+    df['ASA'] = df['ASA_start_date'].notna().astype(int)
+    df['NSAID'] = df['NSAID_start_date'].notna().astype(int)
 
     # Create column for all UGI cancers together (stomach and esophagus)
-    df.loc[:, 'ugica'] = df[['ugica_stomach', 'ugica_esophagus']].max(axis=1)
+    df['ugica'] = df[['ugica_ESCC', 'ugica_EAC', 'ugica_CGC', 'ugica_NCGC']].max(axis=1)
     df.loc[df.ugica.isna(), 'ugica'] = 0
+
+    # Create other outcome variables 
+    df['death'] = df.death_year.notna().astype(int)
+    
+    # Calculate the days between visit_start_date and datetime_dx, and visit_start_date and date_of_death
+    df['days_to_dx'] = (df['datetime_dx'] - df['visit_start_date']).dt.days
+    df['days_to_death'] = (df['date_of_death'] - df['visit_start_date']).dt.days
+
+    # Create the days_to_event column as the minimum of days_to_dx and days_to_death
+    df['days_to_event'] = df[['days_to_dx', 'days_to_death']].min(axis=1)
+    df['months_to_event'] = df['days_to_event'] / 30.4375
 
     return df 
 
-def create_demtable(df, colname_filename, demtable_filename, demtablegrouped_filename):
-
-    # Read in the filename with the renamed column names
-    with open(colname_filename, 'r') as json_file:
-        colname_dict = json.load(json_file)
-        
-    # Create demographic tables
-    mytable = TableOne(df, columns=utils.DEM_TABLE_COLS, categorical=utils.CATEGORICAL_VARS, rename=colname_dict) #, continuous=continuous, groupby=groupby, nonnormal=nonnormal, rename=rename, pval=False)
-    mytable_groupby = TableOne(df, columns=utils.DEM_TABLE_COLS, categorical=utils.CATEGORICAL_VARS, rename=colname_dict, groupby='ugica') #, continuous=continuous, groupby=groupby, nonnormal=nonnormal, rename=rename, pval=False)
-
-    # Save demographic tables
-    mytable.to_csv(demtable_filename)
-    mytable_groupby.to_csv(demtablegrouped_filename)
-
-def main(): 
-
+def parse_args(): 
     # Set up command-line argument parsing 
-    parser = argparse.ArgumentParser(description="Apply exclusion criteria to data file and merge to UGI registry data.")
+    parser = argparse.ArgumentParser(description="Create the cohort by applying inclusion and exclusion criteria to data file and merge to UGI registry data.")
 
     # Add arguments
-    parser.add_argument('cohort_file', type=str, help='The complete cohort data file to apply exclusion criteria to.')
-    parser.add_argument('ugi_file', type=str, help='The UGI registry data file.')
+    parser.add_argument(
+        '--data_txt_file', 
+        type=str, 
+        required=True, 
+        help='Path to text file listing all batched data CSV files'
+    )
+    
+    parser.add_argument(
+        '--ugi_file', 
+        type=str, 
+        required=True, 
+        help='Path to the UGI cancer registry file (CSV format)'
+    )
+    
+    return parser.parse_args()
 
+def main(): 
     # Parse the arguments
-    args = parser.parse_args()
+    args = parse_args()
 
     # Define directory path variable
     data_dir = Path('./../data/')
-    notebook_dir = Path('./../notebook')
-    results_dir = Path('./../results/')
 
     # Define data filenames
-    data_txt_file = 'all_data_files.txt'  # Text file with list of extracted batched data in .csv.gz files
-    ugi_file = args.ugi_file # ie. 'ugicancer_registry_clean.csv'
-
-    # Define renamed column name mapping 
-    colname_filename = Path('./colname_mappings.json')
+    data_txt_file = "all_data_files.txt" 
+    ugi_file = "ugicancer_registry_clean.csv"
 
     # Filenames to be saved as 
     today_date = datetime.now().strftime('%Y%m%d') # Get today's date
+    consort_diagram_numbers_filename = "consort_diagram_numbers.txt"
     final_data_file = f'cleaned_cohort_{today_date}.csv'
-    final_demtable_file = f'demtable_{today_date}.csv'
-    final_demtablegrouped_file = f'demtable_grouped_{today_date}.csv'
 
-    # Apply inclusion criteria 
-    df_original = apply_exclusion_critera(data_dir, data_txt_file)
+    # Get the data filenames
+    file_list = []
+    with open(data_dir / data_txt_file, 'r') as file:
+        file_list = [line.strip() for line in file if line.strip()]
 
-    # Read UGI cancer registry data 
-    df_ugi = pd.read_csv(data_dir / ugi_file, low_memory=False) #, index_col = False)
-
-    # Select the first UGI cancer diagnosis for patients with multiple UGI cancers 
-    df_ugi.sort_values(by='datetime_dx', inplace=True) # Sort duplicates by diagnosis date in ascending order 
-    df_ugi_clean = df_ugi[~df_ugi.mrn.duplicated(keep='first')] # Keep the first row which will be the earliest diagnosis 
-
-    # Merge UGI registry with extracted Epic data 
-    df_merged = df_original.merge(df_ugi_clean, on='mrn', how='left')
-
-    # Apply exclusion criteria and print out the exclusions 
-    df_cohort = apply_exclusion_critera(df_merged.copy())
+    # Apply exclusion criteria 
+    df_cohort = apply_exclusion_criteria(data_dir, file_list, ugi_file, consort_diagram_numbers_filename)
 
     # Clean cohort and define new variables 
     df_clean = clean_data(df_cohort.copy())
 
     # Save final cleaned cohort data
-    df_clean.to_csv(results_dir / final_data_file)
-
-    # Create demographic table 
-    create_demtable(df_clean, colname_filename, results_dir / final_demtable_file, results_dir / final_demtablegrouped_file)
+    df_clean.to_csv(data_dir / final_data_file)
 
 if __name__ == "__main__":
     main()
---
+++ code/create_cohort.sh
@@ -0,0 +1,20 @@
+# Save the current directory
+current_dir=$(pwd)
+
+# Clean up UGI cancer registry data and get cancer subtypes 
+python clean_cancer_registry.py
+
+# Navigate to the ../data directory or exit the script if the directory doesn't exist
+cd ../data || exit
+
+# Run ls | grep to get all the batched data files in one text file 
+# ls | grep -E '^data_20[0-9]{2}-[0-9]{2}-[0-9]{2}_20[0-9]{2}-[0-9]{2}-[0-9]{2}\.csv\.gz$' > all_data_files.txt
+ls | grep -E '^data_2022-[0-9]{2}-[0-9]{2}_2022-[0-9]{2}-[0-9]{2}\.csv\.gz$' > all_data_files.txt
+
+# Return to the original directory 
+cd "$current_dir" || exit
+
+# Merge with UGI registry, apply inclusion criteria, apply exclusion criteria 
+python create_cohort.py \
+    --data_txt_file all_data_files.txt \
+    --ugi_file ugicancer_registry_clean.csv \
